6a2fa341866ba16e58397e06fc3454c5be427dba
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-03-29 01:44:39 +0200

    update

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+// 1. Determine the starting point everytime the mouse is pressed down
+// 2. Determine how far the mouse has traveled relative to the starting point
+// 3. Determine the max distance and the percentage
+// 4. Keeping track how far we have dragged the slider
+// 5. Updating constantly the percentage of the sider
+// 6. Storing the percentage on mouse release
+// 7. Add viewport limitations for the next percentage value (max/min)
+// 8. Looping over images and implement a Parallax effect
+
 const track = document.getElementById("image-track");
 
 const handleOnDown = event => {
